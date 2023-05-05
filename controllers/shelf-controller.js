213b6837e396d23884c99601787bab061f41f7b1
kmakai <makaikwag@gmail.com>
2023-05-04 20:59:07 -0400

    shelves controllers completed

--- controllers/shelf-controller.js
+++ controllers/shelf-controller.js
@@ -5,25 +5,88 @@ const example = asyncHandler(async (req, res, next) => {
 });
 
 const BookShelf = require("../models/shelf-model");
+const Book = require("../models/book-model");
 
 const createBookShelf = asyncHandler(async (req, res, next) => {
-  res.send("create book shelf");
+  const { name } = req.body;
+  const user = req.user;
+
+  const bookShelf = await BookShelf.create({
+    name,
+    user: user._id,
+    description: req.body.description || "",
+  });
+
+  if (!bookShelf) {
+    res.status(400);
+    throw new Error("Book shelf not created");
+  }
+
+  user.myShelves.push(bookShelf._id);
+
+  user.save();
+
+  res.status(201).json({ message: "Book shelf created", bookShelf });
 });
 
 const getBookShelf = asyncHandler(async (req, res, next) => {
-  res.send("get book shelf");
+  const bookShelf = await BookShelf.findById(req.params.id);
+
+  if (!bookShelf) {
+    res.status(400);
+    throw new Error("Book shelf not found");
+  }
+
+  res.status(200).json({ message: "Book shelf found", bookShelf });
 });
 
 const addToBookShelf = asyncHandler(async (req, res, next) => {
-  res.send("add to  book shelf");
+  const bookId = req.body;
+  const bookShelf = await BookShelf.findById(req.params.id);
+  const book = await Book.findById(bookId);
+
+  if (!bookShelf || !book) {
+    res.status(400);
+    throw new Error("Something went wrong");
+  }
+
+  bookShelf.books.push(bookId);
+
+  bookShelf.save();
+
+  res.status(200).json({ message: "Book added to book shelf", bookShelf });
 });
 
 const removeFromBookShelf = asyncHandler(async (req, res, next) => {
-  res.send("remove from  book shelf");
+  const bookId = req.body;
+
+  const bookShelf = await BookShelf.findById(req.params.id);
+
+  if (!bookShelf) {
+    res.status(400);
+    throw new Error("Book shelf not found");
+  }
+
+  bookShelf.books = bookShelf.books.filter(
+    (book) => book.toString() !== bookId
+  );
+
+  bookShelf.save();
+
+  res.status(200).json({ message: "Book removed from book shelf", bookShelf });
 });
 
 const deleteBookShelf = asyncHandler(async (req, res, next) => {
-  res.send("delete book shelf");
+  const bookShelf = await BookShelf.findById(req.params.id);
+
+  if (!bookShelf) {
+    res.status(400);
+    throw new Error("Book shelf not found");
+  }
+
+  bookShelf.remove();
+
+  res.status(200).json({ message: "Book shelf deleted" });
 });
 
 module.exports = {
--- routes/shelf-routes.js
+++ routes/shelf-routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const shelfController = require("../controllers/shelf-controller");
+const authController = require("../controllers/auth-controller");
 
-router.post("/create", shelfController.createBookShelf);
-router.get("/:shelfId", shelfController.getBookShelf);
-router.post("/:shelfId/add", shelfController.addToBookShelf);
-router.post("/:shelfId/remove", shelfController.removeFromBookShelf);
-router.post("/:shelfId/delete", shelfController.deleteBookShelf);
+router.post("/create", authController.protect, shelfController.createBookShelf);
+router.get("/:id", shelfController.getBookShelf);
+router.post("/:id/add", shelfController.addToBookShelf);
+router.post("/:id/remove", shelfController.removeFromBookShelf);
+router.post("/:id/delete", shelfController.deleteBookShelf);
 
 module.exports = router;
